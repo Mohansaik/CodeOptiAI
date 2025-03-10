54a761f229f2a95bcfe18c9a5beaf2ce7ebd2e10
Mohansaik <134529999+Mohansaik@users.noreply.github.com>
2025-03-10 20:16:12 +0530

    Update streamlit_pyspark_optimizer.py

--- python/streamlit_pyspark_optimizer.py
+++ python/streamlit_pyspark_optimizer.py
@@ -5,9 +5,10 @@ from openai import OpenAI
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.', '.')))
 
 class PySparkOptimizerLLMApp:
-    def __init__(self, user_prompt_path, file_type):
-        self.user_prompt_path = user_prompt_path
+    def __init__(self, user_prompt, file_type):
         self.system_prompt_path = './resources/system_prompt_python.txt'  if file_type == "Python" else './resources/system_prompt_sql.txt'
+        self.user_prompt = user_prompt
+        
         self.client = OpenAI(
             base_url="https://models.inference.ai.azure.com",
             api_key=os.getenv("GITHUB_TOKEN"),
@@ -21,7 +22,6 @@ class PySparkOptimizerLLMApp:
             raise FileNotFoundError(f"The file at {file_path} does not exist.")
 
     def run(self):
-        user_prompt = self.user_prompt_path
         system_prompt = self.reader(self.system_prompt_path)
 
         response = self.client.chat.completions.create(
@@ -32,7 +32,7 @@ class PySparkOptimizerLLMApp:
                 },
                 {
                     "role": "user",
-                    "content": user_prompt,
+                    "content": self.user_prompt,
                 },
             ],
             model="gpt-4o-mini",
@@ -41,16 +41,12 @@ class PySparkOptimizerLLMApp:
             top_p=1,
         )
 
-        response_file_path = f"./report/{os.path.basename(self.user_prompt_path).split('.')[0]}_report.txt"
-        with open(response_file_path, 'w') as file:
-            file.write(response.choices[0].message.content)
-
-        return f"Response saved at {response_file_path}"
+        return response.choices[0].message.content
 
 
 if __name__ == "__main__":
     user_prompt = './python_files/user_py_file.py'
     system_prompt = './resources/system_prompt.txt'
 
-    app = PySparkOptimizerLLMApp(user_prompt)
+    app = PySparkOptimizerLLMApp(user_prompt, system_prompt)
     print(app.run())
